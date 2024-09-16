112f68031770b1db633a0eb1ef4e418caaac3f85
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-16 14:56:34 +0300

    Adding "Password form field" as single widget

---
+++ lib/core/widgets/login/password_form_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PasswordFormField extends StatelessWidget {
+  const PasswordFormField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var passwordKey = GlobalKey<FormState>();
+TextEditingController passController=TextEditingController();
+    return TextFormField(
+      key: passwordKey,
+      controller: passController,
+      decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.lock_outline,
+            color: Colors.white,
+          ),
+          focusColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 2.0,
+            ),
+          ),
+          label: Text(
+            'Password',
+            style: TextStyle(color: Colors.white),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          )),
+    );
+  }
+}
