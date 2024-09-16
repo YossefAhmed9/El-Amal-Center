f4875415b0f49c92846ada79266c4a015c45da27
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-16 16:39:40 +0300

    Addng bottom sheet for forgetting password as single widget

---
+++ lib/core/widgets/login/forget_password_bottom_sheet.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constant.dart';
+import '../../utils/styles.dart';
+import 'email_form_field.dart';
+
+class ForgetPasswordBottomSheet extends StatelessWidget {
+  const ForgetPasswordBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height:
+      MediaQuery.of(context).size.height * 0.5,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: AppConstant.primaryColor),
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Forget password',
+              style: Styles.textStyle25(context).copyWith(color: Colors.black,fontWeight: FontWeight.w600),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Text(
+                'Enter your email for verification process we will send 5 digits code to your email',
+                style: Styles.textStyle18(context).copyWith(color: Colors.black,fontWeight: FontWeight.w400),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0,bottom: 14),
+              child: Text('Email',style: Styles.textStyle25(context).copyWith(fontWeight: FontWeight.w600,color: Colors.black),),
+            ),
+            const EmailFormField(borderColor: Colors.black,iconColor: AppConstant.defaultColor,),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
