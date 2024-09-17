9ae9be2c40f9efca1460ed980d8d2e486a0d4cc0
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-17 19:42:40 +0300

    Adding color property

--- lib/core/widgets/login/sign_in_button.dart
+++ lib/core/widgets/login/sign_in_button.dart
@@ -2,24 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:markaz_elamal/core/utils/constant.dart';
 
 class SignInButton extends StatelessWidget {
-  const SignInButton({Key? key}) : super(key: key);
-
+  const SignInButton({Key? key, required this.buttonColor, required this.textColor}) : super(key: key);
+final Color buttonColor;
+final Color textColor;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10.0),
-      child: Container(width: 200,height: 45,
-          decoration: BoxDecoration(color: AppConstant.primaryColor,borderRadius: BorderRadius.circular(15),),
+      padding: const EdgeInsets.only(top: 27.0),
+      child: Container(width: 300,height: 45,
+          decoration: BoxDecoration(color: buttonColor,borderRadius: BorderRadius.circular(15),),
           child: MaterialButton(
             onPressed: (){},
-            child: const Text(
+            child:  Text(
               'Sign In',
               style: TextStyle(
-                  color: AppConstant.defaultColor,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500),
+                  color: textColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600),
             ),
-            ),
+          ),
       ),
     );
   }
