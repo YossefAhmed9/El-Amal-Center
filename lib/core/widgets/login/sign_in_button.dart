9a34406a4d5d2af070bc5d8e398b7ee0c5e6c211
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-17 20:01:16 +0300

    Adding buttonWidth, buttonHeight, buttonText properties

--- lib/core/widgets/login/sign_in_button.dart
+++ lib/core/widgets/login/sign_in_button.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:markaz_elamal/core/utils/constant.dart';
 
 class SignInButton extends StatelessWidget {
-  const SignInButton({Key? key, required this.buttonColor, required this.textColor}) : super(key: key);
+  const SignInButton({Key? key, required this.buttonColor, required this.textColor, required this.buttonWidth, required this.buttonText, this.buttonHeight}) : super(key: key);
 final Color buttonColor;
 final Color textColor;
+final double? buttonWidth;
+final double? buttonHeight;
+final String buttonText;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 27.0),
-      child: Container(width: 300,height: 45,
+      padding:  const EdgeInsets.only(top: 27.0),
+      child: Container(width: buttonWidth,height: buttonHeight,
           decoration: BoxDecoration(color: buttonColor,borderRadius: BorderRadius.circular(15),),
           child: MaterialButton(
             onPressed: (){},
             child:  Text(
-              'Sign In',
+              buttonText,
               style: TextStyle(
                   color: textColor,
                   fontSize: 24,
