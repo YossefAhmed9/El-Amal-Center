eb504cedbc98c95b8e860205a5cd136b8028a720
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-17 19:45:02 +0300

    Adding color iconColor,borderColor properties

--- lib/core/widgets/login/email_form_field.dart
+++ lib/core/widgets/login/email_form_field.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmailFormField extends StatelessWidget {
-  const EmailFormField({Key? key}) : super(key: key);
-
+  const EmailFormField({Key? key, required this.borderColor, required this.iconColor}) : super(key: key);
+final Color borderColor;
+final Color iconColor;
   @override
   Widget build(BuildContext context) {
     var emailKey = GlobalKey<FormState>();
@@ -12,24 +13,24 @@ class EmailFormField extends StatelessWidget {
       key: emailKey,
       controller: emailController,
       decoration: InputDecoration(
-          prefixIcon: Icon(
+          prefixIcon:  Icon(
             Icons.mail_outline,
-            color: Colors.white,
+            color: iconColor,
           ),
           focusColor: Colors.white,
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: BorderSide(
-              color: Colors.white,
-              width: 2.0,
+            borderRadius: BorderRadius.circular(14.0),
+            borderSide:  BorderSide(
+              color: borderColor,
+              width: 1.0,
             ),
           ),
-          label: Text(
+          label: const Text(
             'Email',
             style: TextStyle(color: Colors.white),
           ),
           border: OutlineInputBorder(
-            borderSide: BorderSide(
+            borderSide: const BorderSide(
               color: Colors.red,
             ),
             borderRadius: BorderRadius.circular(20),
