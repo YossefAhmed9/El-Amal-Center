430552d77f0cf4f5266db2c51f5ab87c4189686d
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-09-17 19:45:52 +0300

    Refactoring code  and styles

--- lib/core/widgets/login/form.dart
+++ lib/core/widgets/login/form.dart
@@ -5,17 +5,18 @@ import 'package:markaz_elamal/core/widgets/login/sign_in_button.dart';
 import '../../utils/constant.dart';
 import '../../utils/styles.dart';
 import 'email_form_field.dart';
+import 'forget_password_bottom_sheet.dart';
 
 class FormWithEmailAndPassword extends StatelessWidget {
   const FormWithEmailAndPassword({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 50, bottom: 50,left: 40),
+          padding: const EdgeInsets.only(top: 50, bottom: 50, left: 40),
           child: SizedBox(
             width: 200,
             height: 72,
@@ -28,7 +29,7 @@ class FormWithEmailAndPassword extends StatelessWidget {
           child: Container(
             clipBehavior: Clip.antiAliasWithSaveLayer,
             width: 360,
-            height: MediaQuery.of(context).size.height*0.47,
+            height: MediaQuery.of(context).size.height * 0.47,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
                 color: AppConstant.primaryColor),
@@ -43,32 +44,41 @@ class FormWithEmailAndPassword extends StatelessWidget {
                     borderRadius: BorderRadius.circular(40)),
                 child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset('assets/images/markaz_elamal.png',height: 94.03,width: 74,),
+                    //الصورة
+                    Image.asset(
+                      'assets/images/markaz_elamal.png',
+                      height: 94.03,
+                      width: 74,
                     ),
-
+                    //الكلام نفسه
                     Padding(
                       padding: const EdgeInsets.only(bottom: 10.0),
                       child: Text(
                         'Markaz ElAmal',
-                        style: Styles.textStyle16(context)
-                            .copyWith(color: Colors.white, fontFamily: 'Peralta'),
+                        style: Styles.textStyle16(context).copyWith(
+                            color: Colors.white, fontFamily: 'Peralta'),
                       ),
                     ),
-                    const EmailFormField(),
-                    const SizedBox(height: 20,),
-                    const PasswordFormField(),
-                    const SignInButton(),
-                    TextButton(
-                        onPressed: (){},
-                        child: const Text(
-                          'Forgot password?',
-                          style: TextStyle(
-                              color: AppConstant.primaryColor,fontSize: 20),
-                        ),
+                    const EmailFormField(borderColor: AppConstant.primaryColor,iconColor: AppConstant.primaryColor,),
+                    const SizedBox(
+                      height: 16,
                     ),
-
+                    const PasswordFormField(),
+                    const SignInButton(buttonColor: AppConstant.primaryColor,textColor: AppConstant.defaultColor,),
+                    TextButton(
+                      onPressed: () {
+                        showBottomSheet(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const ForgetPasswordBottomSheet();
+                            });
+                      },
+                      child: Text(
+                        'Forgot password?',
+                        style: Styles.textStyle16(context)
+                            .copyWith(fontWeight: FontWeight.w600),
+                      ),
+                    ),
                   ],
                 ),
               ),
