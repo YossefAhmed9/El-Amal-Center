e63caa39cfa9a269a84723a7c3be04cfb9298033
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-01 01:17:36 +0300

    Making doctors ListView as single widget

---
+++ lib/core/widgets/home_page_widgets/doctors_list_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constant.dart';
+import 'doctor_card.dart';
+
+class DoctorsListView extends StatelessWidget {
+  const DoctorsListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 318,
+        width: double.infinity,
+        child:ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 12,bottom: 15),
+                child: Container(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  width: 270,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppConstant.defaultColor),
+                  child: const DoctorCard(),
+                ),
+              );
+            }),
+    );
+  }
+}
