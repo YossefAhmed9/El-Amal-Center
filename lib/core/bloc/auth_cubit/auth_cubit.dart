685e8d73601fcadcb5acfff7d4fbf9148460444c
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-01 01:15:07 +0300

    Adding Authorization cubit

---
+++ lib/core/bloc/auth_cubit/auth_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'auth_states.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  AuthCubit() : super(AuthInitState());
+
+  static AuthCubit get(context) => BlocProvider.of(context);
+
+
+
+
+
+
+}
