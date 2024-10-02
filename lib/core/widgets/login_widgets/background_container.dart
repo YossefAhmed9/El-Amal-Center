e00f8983feb42d7e5f73dca4d5806f50efe9526f
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:07:51 +0300

    Refactoring

--- lib/core/widgets/login_widgets/background_container.dart
+++ lib/core/widgets/login_widgets/background_container.dart
@@ -8,16 +8,18 @@ class BackgroundContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.65,
-        decoration: const BoxDecoration(
-          color: AppConstant.primaryColor,
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(65), topLeft: Radius.circular(65)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children:[
+        Container(
+          height: MediaQuery.of(context).size.height * 0.65,
+          decoration: const BoxDecoration(
+            color: AppConstant.primaryColor,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(65), topLeft: Radius.circular(65)),
+          ),
         ),
-      ),
+      ]
     );
   }
 }
