4c284393b386cac5299d4c747a9415ed3bc0acc6
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:07:00 +0300

    adding home cubit

---
+++ lib/core/bloc/home_cubit/home_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../features/home_layout/book_view/book_view.dart';
+import '../../../features/home_layout/category_view/category_view.dart';
+import '../../../features/home_layout/home_page/home_page_view.dart';
+import '../../../features/home_layout/profile_view/profile_view.dart';
+import 'home_states.dart';
+
+class HomeCubit extends Cubit<HomeStates> {
+  HomeCubit() : super(HomeInitState());
+
+  static HomeCubit get(context) => BlocProvider.of(context);
+
+
+  List BottomNavBarscreens=[
+    const HomePageView(),
+    const CategoryView(),
+    const BookView(),
+    const ProfileView()
+  ];
+
+
+  int navBarIndex=0;
+  void changeBottomNavBarIndex(int index){
+    navBarIndex=index;
+    emit(HomeChangeBottomNavBarIndex());
+  }
+
+  void GoToCategory(){
+    navBarIndex=1;
+    emit(GoToCategoryView());
+
+  }
+
+}
