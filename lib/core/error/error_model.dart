b52d7325ed9a43062a4ef094c138ef1826292da9
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:02:14 +0300

    first commit

--- lib/core/error/error_model.dart
+++ lib/core/error/error_model.dart
@@ -1,16 +1,15 @@
-
-import '../api/end_point.dart';
-
-class ErrorModel{
-  final int status;
-  final String errorMessage;
-
-  ErrorModel({required this.status, required this.errorMessage});
-
-  factory ErrorModel.fromJson(Map<String,dynamic>jsonData){
-    return ErrorModel(
-        status: jsonData[ApiKeys.status],
-        errorMessage: jsonData[ApiKeys.errorMessage]);
-
-  }
-}
+//
+//
+// class ErrorModel{
+//   final int status;
+//   final String errorMessage;
+//
+//   ErrorModel({required this.status, required this.errorMessage});
+//
+//   factory ErrorModel.fromJson(Map<String,dynamic>jsonData){
+//     return ErrorModel(
+//         status: jsonData[ApiKeys.status],
+//         errorMessage: jsonData[ApiKeys.errorMessage]);
+//
+//   }
+// }
