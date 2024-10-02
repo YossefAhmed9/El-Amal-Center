40d97d3571e8d352c8c674240fe21fa429b81b39
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:01:11 +0300

    adding center's name and logo path  as variables

--- lib/core/utils/constant.dart
+++ lib/core/utils/constant.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppConstant {
   static const Color primaryColor = Colors.white;
   static const Color defaultColor = Color(0XFF336EA6);
-
+static const String centerName='markaz elamal';
+static const String logoPath='assets/images/markaz_elamal.png';
 }
