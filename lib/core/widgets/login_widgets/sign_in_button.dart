af952df2c7413d61e4b7834c836ddb63d766b8bc
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:03:45 +0300

    building sign in button as single widget

--- lib/core/widgets/login_widgets/sign_in_button.dart
+++ lib/core/widgets/login_widgets/sign_in_button.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:markaz_elamal/core/utils/constant.dart';
 
 class SignInButton extends StatelessWidget {
-  const SignInButton({Key? key, required this.buttonColor, required this.textColor, required this.buttonWidth, required this.buttonText, this.buttonHeight}) : super(key: key);
+  const SignInButton({super.key, required this.buttonColor, required this.textColor, required this.buttonWidth, required this.buttonText, this.buttonHeight, required this.onPress});
 final Color buttonColor;
 final Color textColor;
 final double? buttonWidth;
 final double? buttonHeight;
 final String buttonText;
+final Function() onPress;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,14 +16,17 @@ final String buttonText;
       child: Container(width: buttonWidth,height: buttonHeight,
           decoration: BoxDecoration(color: buttonColor,borderRadius: BorderRadius.circular(15),),
           child: MaterialButton(
-            onPressed: (){},
+            onPressed: onPress,
             child:  Text(
               buttonText,
               style: TextStyle(
                   color: textColor,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600),
+                  fontSize: MediaQuery.of(context).size.height*0.025,
+                  fontWeight: FontWeight.w600,
+
+              ),
             ),
+
           ),
       ),
     );
