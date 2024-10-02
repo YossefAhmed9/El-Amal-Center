ec6eb46735b1ea1fe0d2d9dd00ccd49ea10c9f4e
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:07:40 +0300

    Refactoring auth view

--- lib/features/authentication_view/authincation.dart
+++ lib/features/authentication_view/authincation.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:markaz_elamal/core/bloc/auth_cubit/auth_cubit.dart';
+import '../../core/bloc/auth_cubit/auth_states.dart';
 import '../../core/utils/constant.dart';
 import '../../core/widgets/login_widgets/background_container.dart';
 import '../../core/widgets/login_widgets/form.dart';
@@ -9,24 +12,31 @@ class Authentication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      backgroundColor: AppConstant.defaultColor,
+    return BlocConsumer<AuthCubit, AuthStates>(
+      listener: (context, state) {
+      },
+      builder: (context, state) {
+        AuthCubit cubit=AuthCubit.get(context);
+        return const Scaffold(
+          backgroundColor: AppConstant.defaultColor,
 
-      body: SafeArea(
-        child:  Stack(
-          children: [
-            BackgroundContainer(),
-            Column(
-              mainAxisSize: MainAxisSize.max,
+          body: SafeArea(
+            child: Stack(
               children: [
-                LoginForm(),
-                Spacer(),
-                NoAccount(),
+                BackgroundContainer(),
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    LoginForm(),
+                    Spacer(),
+                    NoAccount(),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
