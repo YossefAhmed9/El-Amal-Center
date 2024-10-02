80f99b674fcaa0719b45d26f582e866bc704ffc6
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 00:07:14 +0300

    adding auth cubit

--- lib/core/bloc/auth_cubit/auth_cubit.dart
+++ lib/core/bloc/auth_cubit/auth_cubit.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../features/home_layout/home_page/home_page_view.dart';
 import 'auth_states.dart';
 
 class AuthCubit extends Cubit<AuthStates> {
@@ -7,8 +9,13 @@ class AuthCubit extends Cubit<AuthStates> {
 
   static AuthCubit get(context) => BlocProvider.of(context);
 
-
-
+  void navigateToHome(BuildContext context){
+    Navigator.push(
+      context, MaterialPageRoute(
+        builder: (context) =>const HomePageView()),
+    );
+    emit(GoToHome());
+  }
 
 
 
