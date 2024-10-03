249d12d31c30a1dd72ee49be10fddc4eaf8f07c4
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-10-03 23:05:20 +0300

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:markaz_elamal/core/api/dio_helper.dart';
 import 'package:markaz_elamal/core/bloc/auth_cubit/auth_cubit.dart';
